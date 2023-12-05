5cef2bd0ee4d8f9842c15e14619af0f14fbd3000
azam <azamdeve939@gmail.com>
2023-12-05 19:03:23 +0500

    some change hospital controller

--- api/controllers/hospital.js
+++ api/controllers/hospital.js
@@ -32,18 +32,82 @@ exports.createHospital = async (req, res) => {
       hospital
     });
   };
-// exports.createHospital = async (req, res)=>{
-//     const {fullname, email, phonenumber,address,branch} = req.body;
-//        const hospital = await Hospital({
-//         fullname,
-//         email,
-//         phonenumber,
-//         address, 
-//         branch
-//        });
-//        await hospital .save();
-//        res.json({
-//         success:true,hospital
-//        });
-// } ;
+// Get All Hospitals
+exports.getAllHospitals = async (req, res) => {
+  try {
+    const hospitals = await Hospital.find();
+    res.status(200).json({ hospitals });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Get Single Hospital by ID
+exports.getSingleHospital = async (req, res) => {
+  try {
+    const { hospitalId } = req.params;
+
+    const hospital = await Hospital.findById(hospitalId);
+
+    if (!hospital) {
+      return res.status(404).json({ error: "Hospital not found" });
+    }
+
+    res.status(200).json({ hospital });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+//update
+exports.updateHospital = async (req, res) => {
+  try {
+    const { hospitalId } = req.params;
+    const updatedData = req.body;
+
+    // Process file upload if available
+    if (req.file) {
+      console.log("File received:", req.file);
+      const result = await cloudinary.uploader.upload(req.file.path);
+      updatedData.avatar = result.secure_url;
+    } else {
+      console.log("No file received");
+    }
+
+    const updatedHospital = await Hospital.findByIdAndUpdate(
+      hospitalId,
+      updatedData,
+      { new: true }
+    );
+
+    if (!updatedHospital) {
+      return res.status(404).json({ message: "Hospital not found" });
+    }
+
+    return res.status(200).json({ message: "Hospital updated successfully", updatedHospital });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error", details: error.message });
+  }
+};
+
+// Delete Hospital by ID
+exports.deleteHospital = async (req, res) => {
+  try {
+    const { hospitalId } = req.params;
+
+    const deletedHospital = await Hospital.findByIdAndDelete(hospitalId);
+
+    if (!deletedHospital) {
+      return res.status(404).json({ message: "Hospital not found" });
+    }
+
+    return res.status(200).json({ message: "Hospital deleted successfully", deletedHospital });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 
--- api/routes/hospital_routes.js
+++ api/routes/hospital_routes.js
@@ -6,97 +6,25 @@ const storage = multer.diskStorage({});
 
 const upload = multer({ storage });
 
-const { createHospital, getHospital } = require("../controllers/hospital");
+const { createHospital, getAllHospitals,getSingleHospital,updateHospital,deleteHospital } = require("../controllers/hospital");
 
 router.post("/create-hospital",
   upload.single("avatar"),
 createHospital);
 
 //get api all hospital.
-router.get("/all-hospitals", async (req, res) => {
-    try {
-      const data = await Hospital.find();
-      res.json({ success: true, data });
-    } catch (error) {
-      console.error("Error fetching hospitals:", error);
-      res.status(500).json({ success: false, message: "An error occurred." });
-    }
-  });
+router.get("/all-hospitals",getAllHospitals)
   
   //get single hospital
-  router.get("/hospital/:hospitalId", async (req, res) => {
-    try {
-      const hospitalId = req.params.hospitalId;
+  router.get("/hospital/:hospitalId",getSingleHospital) 
   
-      // Use findById to find a hospital by its ID
-      const hospital = await Hospital.findById(hospitalId);
-  
-      if (!hospital) {
-        return res
-          .status(404)
-          .json({ success: false, message: "Hospital not found." });
-      }
-  
-      res.json({ success: true, data: hospital });
-    } catch (error) {
-      console.error("Error fetching hospital:", error);
-      res.status(500).json({ success: false, message: "An error occurred." });
-    }
-  });
 
   //Update api hospital
-router.patch('/update-hospital/:hospitalId', upload.single('avatar'), async (req, res) => {
-    try {
-      const { hospitalId } = req.params;
-      const updatedData = req.body;
-  
-      if (req.file) {
-        const result = await cloudinary.uploader.upload(req.file.path);
-        updatedData.avatar = result.secure_url;
-      }
-  
-      // Find the hospital by ID and update its information
-      const updatedHospital = await Hospital.findByIdAndUpdate(
-        hospitalId,
-        updatedData,
-        { new: true }
-      );
-  
-      if (!updatedHospital) {
-        return res.status(404).json({ message: 'Hospital not found' });
-      }
-  
-      return res.status(200).json({ message: 'Hospital updated successfully', updatedHospital });
-    } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'Internal server error', details: error.message });
-    }
-  });
+router.patch('/update-hospital/:hospitalId', upload.single('avatar'),updateHospital )
+
   
   ///Delete api hospital
-router.delete("/delete-hospital/:id", async (req, res) => {
-    const hospitalId = req.params.id;
-  
-    try {
-      const deletedHospital = await Hospital.findByIdAndDelete(hospitalId);
-      if (!deletedHospital) {
-        return res.status(404).json({
-          success: false,
-          message: "Hospital not found."
-        });
-      }
-      res.json({
-        success: true,
-        message: "Hospital deleted successfully."
-      });
-    } catch (error) {
-      console.error("Error deleting hospital:", error);
-      res.status(500).json({
-        success: false,
-        message: "An error occurred while deleting the hospital."
-      });
-    }
-  });
+router.delete("/delete-hospital/:id", deleteHospital)
 
 
   module.exports = router;
